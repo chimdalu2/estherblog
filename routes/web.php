892f3262cddca4efdf3b86f6e10957b9b90043f7
esther <estherchidialu@gmail.com>
2025-10-15 22:59:54 +0100

    renaming contact.blade.php to contacts.blade.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ContactsController;
 
 
-Route::get("/contacts", [ContactController::class, "show"])->name("contacts");
-Route::post("/contacts", [ContactController::class, "send"])->name("contacts.send");
+Route::get("/contacts", [ContactsController::class, "show"])->name("contacts");
+Route::post("/contacts", [ContactsController::class, "send"])->name("contacts.send");
